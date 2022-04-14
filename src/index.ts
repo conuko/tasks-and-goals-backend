9d03d9763d4b15ce978590f8cfd0da6e0bb1b88c
conuko <constantinu93@gmail.com>
2022-04-14 19:12:31 +0200

    fix: minor fixes at server

--- src/index.ts
+++ src/index.ts
@@ -79,6 +79,7 @@ app.delete(`/task/:id`, async (req, res) => {
   res.json(task);
 });
 
-app.listen(3000, () =>
-  console.log("REST API server ready at: http://localhost:3000")
-);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
