96bbbb85fe7570522a45d0f46b67802208d9be77
conuko <constantinu93@gmail.com>
2022-04-27 15:59:23 +0200

    feat: add cors options to server

--- src/index.js
+++ src/index.js
@@ -13,7 +13,13 @@ app.use(express.json());
 
 require("dotenv").config();
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
